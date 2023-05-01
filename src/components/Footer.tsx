02c47033596053bfa27a391a10befb5f655fb09a
= <innocentmisheck03@gmail.com>
2023-05-01 08:10:30 +0200

    Commit Changes

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return <footer className="text-center py-6 font-medium">21st Generation | Copyright @ 2023</footer>;
+};
+
+export default Footer;
